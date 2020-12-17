7fe9c723f6a146fcd4ca83c13f108744cf2f96c1
dev01997D <devnandan01997@gmail.com>
2020-12-18 00:49:48 +0530

    Added: Ability to validate the user name taken from input field of angular form and throw a proper message

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl="";
   url;
   userName: string = "";
+  nameError: string ="";
 
   //Like constructor, initialize first. Life cycle event
   ngOnInit(): void {
@@ -21,5 +22,18 @@ export class AppComponent {
   onClick($event){
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event) {
+    console.log("Change Event Occured!", $event.data);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    else {
+      this.nameError="Name is Incorrect";
+      return;
+    }
   }
 }
